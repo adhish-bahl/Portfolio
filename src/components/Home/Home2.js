d82d20ab973c1ffae68a3ead72498645b57c2826
Adhish Bahl <adhish.bahl@cloudurity.com>
2024-08-14 17:20:41 +0530

    major pages done

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I'm Adhish Bahl, pursuing Masters of Computer Applications (MCA) from Christ University, Bangalore.
+              <br />
+              <br />I am an aspiring Full Stack Web Developer, keen to work on tools like <b className="purple"> HTML5, CSS3, JavaScript, ReactJS, NodeJS, NextJS, TypeScript and MySQL.</b> I love upskilling in new technologies and am constantly looking for opportunities to learn.
+              <br />
+              <br />
+              I am also a sports person who enjoys <b className="purple">football and swimming.</b>
+              <br />
+              <br />
+              An absolute tech enthusiast, I am always up for conversations on all things technology. Let's connect!
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/adhish-bahl/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/adhishbahl/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
